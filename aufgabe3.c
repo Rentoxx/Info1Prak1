84fdf79dc5f99fd88ac56dd0dbd9c107348b9630
Rentoxx <thomas9.9.935@gmail.com>
2023-11-07 09:39:04 +0100

    added A3

--- aufgabe3.c
+++ aufgabe3.c
@@ -3,21 +3,11 @@ int differenz(int a, int b) {
     return a-b;
 }
 
-int absolut(int a) {
-    if(a < 0) {
-        a = a*(-1);
-        return a;
-    }else{
-        return a;
-    }
-}
-
 int main() {
-    int a = 5;
-    int b = 8;
+    int a = 42;
+    int b = 41;
     int c = differenz(a,b);
-    int d = absolut(c);
 
-    printf("Die Differenz von %d und %d ist %d. Der Abstand ist %d\n", a, b, c, d);
+    printf("%d - %d = %d\n", a, b, c);
     return 0;
 }
